654c9ee10db5d5f93f1b34b47893b2542be79fc3
Sam Soni <sam.07soni@gmail.com>
2026-04-20 23:52:28 +0530

    fix: resolve AdSense data-nscript validation error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,12 +59,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script
+        <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9834734153117480"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
+        ></script>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-HVPE9V9P77"
           strategy="afterInteractive"
